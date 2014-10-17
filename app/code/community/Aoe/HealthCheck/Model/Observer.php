7b12ed4c5dcf247d7142546d8bda06f1906053c7
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-10-17 03:51:21 +0000

    Added hack to avoid redirect

---
+++ app/code/community/Aoe/HealthCheck/Model/Observer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class Aoe_HealthCheck_Model_Observer
+ *
+ * @author Fabrizio Branca
+ * @since 2014-10-16
+ */
+class Aoe_HealthCheck_Model_Observer {
+
+    /**
+     * Little hack to make sure web/url/redirect_to_base is set to false for this controller in any case.
+     * Otherwise Magento would redirect to the base url which would result into an 302 status code instead of a 200 which in turn
+     * wouldn't be detected as a healthy instance.
+     *
+     * Check Mage_Core_Controller_Varien_Front->_checkBaseUrl()
+     *
+     * This method is called in event 'controller_front_init_routers' which is the closest event to _checkBaseUrl
+     */
+    public function disableRedirectToBase() {
+        $originalPathInfo = Mage::app()->getRequest()->getOriginalPathInfo();
+        if (strpos($originalPathInfo, '/health/check/') === 0) {
+            $redirectCode = (int)Mage::getStoreConfig('web/url/redirect_to_base');
+            if ($redirectCode) {
+                Mage::app()->getStore(null)->setConfig('web/url/redirect_to_base', false);
+            }
+        }
+    }
+
+}
